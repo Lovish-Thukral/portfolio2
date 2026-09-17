ddf5b7760b7fa73b142e02da7edf43cdc7a1e473
lovish <Lovish>
2026-09-17 20:50:41 +0530

    added config for netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-
+import netlify from "@netlify/vite-plugin-tanstack-start";
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  plugins: [netlify()],
+
   tanstackStart: {
-    
-    server: { entry: "server" },
+    server: {
+      entry: "server",
+    },
   },
 });
